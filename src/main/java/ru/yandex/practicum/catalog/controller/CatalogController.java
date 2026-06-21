126cf8cce12c6f37dd5676c91eef1b240cf0993a
kirshumir01 <kirshumir@yandex.ru>
2026-06-21 10:34:38 +0300

    YNDX: реализация CatalogController, CatalogService (branch: module_two_six_branch)

--- src/main/java/ru/yandex/practicum/catalog/controller/CatalogController.java
+++ src/main/java/ru/yandex/practicum/catalog/controller/CatalogController.java
@@ -7,8 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.result.view.Rendering;
+import reactor.core.publisher.Mono;
 import ru.yandex.practicum.catalog.service.CatalogService;
-import ru.yandex.practicum.items.dto.ItemsPageDto;
 import ru.yandex.practicum.items.model.ItemSort;
 
 @Validated
@@ -19,19 +20,18 @@ public class CatalogController {
     private final CatalogService catalogService;
 
     @GetMapping({"/", "/items"})
-    public String getItems(
+    public Mono<Rendering> getItems(
             @RequestParam(name = "search", defaultValue = "") String search,
             @RequestParam(name = "sort", defaultValue = "NO") ItemSort sort,
             @RequestParam(name = "pageNumber", defaultValue = "1") @Positive int pageNumber,
             @RequestParam(name = "pageSize", defaultValue = "5") @Positive int pageSize,
             Model model) {
-        ItemsPageDto page = catalogService.getItems(search, sort, pageNumber, pageSize);
-
-        model.addAttribute("items", page.getItems());
-        model.addAttribute("search", search);
-        model.addAttribute("sort", sort);
-        model.addAttribute("paging", page.getPaging());
-
-        return "items";
+        return catalogService.getItems(search, sort, pageNumber, pageSize)
+                .map(page -> Rendering.view("items")
+                        .modelAttribute("items", page.getItems())
+                        .modelAttribute("search", search)
+                        .modelAttribute("sort", sort)
+                        .modelAttribute("paging", page.getPaging())
+                        .build());
     }
 }
--- src/main/java/ru/yandex/practicum/catalog/service/CatalogService.java
+++ src/main/java/ru/yandex/practicum/catalog/service/CatalogService.java
@@ -1,9 +1,10 @@
 package ru.yandex.practicum.catalog.service;
 
+import reactor.core.publisher.Mono;
 import ru.yandex.practicum.items.dto.ItemsPageDto;
 import ru.yandex.practicum.items.model.ItemSort;
 
 public interface CatalogService {
 
-    ItemsPageDto getItems(String search, ItemSort sort, int pageNumber, int pageSize);
+    Mono<ItemsPageDto> getItems(String search, ItemSort sort, int pageNumber, int pageSize);
 }
