5fe4ca58d4c104cf5fc76b270057d1caf74b864f
kirshumir01 <kirshumir@yandex.ru>
2026-06-24 04:35:22 +0300

    YNDX: реализация ItemCacheService в модуле market-app (branch: module_two_seven_branch)

--- market-app/src/main/java/ru/yandex/practicum/cache/ItemCacheService.java
+++ market-app/src/main/java/ru/yandex/practicum/cache/ItemCacheService.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.cache;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,10 +7,8 @@ import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import ru.yandex.practicum.dto.cache.ItemCardCacheDto;
-import ru.yandex.practicum.dto.cache.ItemListCacheDto;
 
 import java.time.Duration;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -29,23 +26,9 @@ public class ItemCacheService {
                 .map(value -> objectMapper.convertValue(value, ItemCardCacheDto.class));
     }
 
-    public Mono<List<ItemListCacheDto>> getItemList() {
-        return redisTemplate.opsForValue()
-                .get("product:list")
-                .map(value -> objectMapper.convertValue(
-                        value,
-                        new TypeReference<List<ItemListCacheDto>>() {}
-                ));
-    }
-
     public Mono<Boolean> saveItemCard(ItemCardCacheDto dto) {
         return redisTemplate.opsForValue()
                 .set("product:card:" + dto.getId(), dto, cacheTtl);
-    }
-
-    public Mono<Boolean> saveItemList(List<ItemListCacheDto> products) {
-        return redisTemplate.opsForValue()
-                .set("product:list", products, cacheTtl);
     }
 
     public Mono<Long> evictItemLists() {
