9c5817b0cf021173a608ade12d2471d296b692da
kirshumir01 <kirshumir@yandex.ru>
2026-07-05 16:17:39 +0300

    YNDX: реализация обработки заказа индивидуально для авторизированного пользователя (branch: module_two_eight_branch)

--- market-app/src/main/java/ru/yandex/practicum/controller/PurchaseController.java
+++ market-app/src/main/java/ru/yandex/practicum/controller/PurchaseController.java
@@ -3,9 +3,12 @@ package ru.yandex.practicum.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 import ru.yandex.practicum.exception.BadRequestException;
 import ru.yandex.practicum.service.OrderService;
+
+import java.security.Principal;
 
 @Controller
 @AllArgsConstructor
@@ -14,12 +17,12 @@ public class PurchaseController {
     private final OrderService orderService;
 
     @PostMapping("/buy")
-    public Mono<String> buy() {
-        return orderService.createOrderFromCart()
-                .map(orderId -> "redirect:/orders/" + orderId + "?newOrder=true")
-                .onErrorReturn(
-                        BadRequestException.class,
-                        "redirect:/cart/items?paymentError=true"
-                );
+    public Mono<String> buy(ServerWebExchange exchange) {
+        return exchange.getPrincipal()
+                .map(Principal::getName)
+                .flatMap(username -> orderService.createOrderFromCart(username)
+                        .map(orderId -> "redirect:/orders/" + orderId + "?newOrder=true"))
+                .onErrorResume(BadRequestException.class,
+                        ex -> Mono.just("redirect:/cart/items?paymentError=true"));
     }
 }
