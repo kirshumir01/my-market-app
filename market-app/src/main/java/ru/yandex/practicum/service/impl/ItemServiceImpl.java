4ea8167449521e5405ac713e20b1f1dd45f25f73
kirshumir01 <kirshumir@yandex.ru>
2026-07-05 16:12:19 +0300

    YNDX: реализация получения информации о товаре индивидуально для авторизированного пользователя на уровнях контроллера и сервиса (branch: module_two_eight_branch)

--- market-app/src/main/java/ru/yandex/practicum/service/impl/ItemServiceImpl.java
+++ market-app/src/main/java/ru/yandex/practicum/service/impl/ItemServiceImpl.java
@@ -7,32 +7,44 @@ import reactor.core.publisher.Mono;
 import ru.yandex.practicum.cache.ItemCacheService;
 import ru.yandex.practicum.dto.item.ItemDto;
 import ru.yandex.practicum.dto.item.ItemRequest;
+import ru.yandex.practicum.exception.NotFoundException;
 import ru.yandex.practicum.mapper.ItemMapper;
 import ru.yandex.practicum.model.CartItem;
 import ru.yandex.practicum.model.Item;
+import ru.yandex.practicum.model.User;
 import ru.yandex.practicum.repository.CartItemRepository;
 import ru.yandex.practicum.repository.ItemRepository;
+import ru.yandex.practicum.repository.UserRepository;
 import ru.yandex.practicum.service.ItemService;
 
 @Service
 @RequiredArgsConstructor
 public class ItemServiceImpl implements ItemService {
+
+
     private final ItemRepository itemRepository;
     private final CartItemRepository cartItemRepository;
     private final ItemCacheService cacheService;
+    private final UserRepository userRepository;
 
     @Override
     @Transactional(readOnly = true)
-    public Mono<ItemDto> getItem(long itemId) {
+    public Mono<ItemDto> getItem(String username, long itemId) {
         return cacheService.getItemCardCached(itemId)
-                .flatMap(itemCard -> cartItemRepository.findByItemId(itemId)
-                        .map(CartItem::getCount)
-                        .defaultIfEmpty(0)
-                        .map(count -> {
-                            ItemDto itemDto = ItemMapper.toItemDto(itemCard);
-                            itemDto.setCount(count);
-                            return itemDto;
-                        }));
+                .flatMap(itemCard -> {
+                    Mono<Integer> countMono = username == null || username.isBlank()
+                            ? Mono.just(0)
+                            : getUserId(username)
+                            .flatMap(userId -> cartItemRepository.findByUserIdAndItemId(userId, itemId))
+                            .map(CartItem::getCount)
+                            .defaultIfEmpty(0);
+
+                    return countMono.map(count -> {
+                        ItemDto itemDto = ItemMapper.toItemDto(itemCard);
+                        itemDto.setCount(count);
+                        return itemDto;
+                    });
+                });
     }
 
     @Override
@@ -51,5 +63,13 @@ public class ItemServiceImpl implements ItemService {
                         cacheService.evictItemLists()
                                 .thenReturn(ItemMapper.toItemDto(savedItem))
                 );
+    }
+
+    private Mono<Long> getUserId(String username) {
+        return userRepository.findByUsername(username)
+                .switchIfEmpty(Mono.error(
+                        new NotFoundException("User with username = %s not found".formatted(username))
+                ))
+                .map(User::getId);
     }
 }
